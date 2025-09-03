107f21ad0dd7689d9d8cfa96d90dfc0b0cc8042c
Jiroh Carpio <jirohreinier.carpio@cvsu.edu.ph>
2025-09-04 01:53:13 +0800

    Completed the BOSS Level Simon Game on my own with minimal help from AI and from the actual course. Will try to the the challenge again on my own.

---
+++ Course Sections/Section 20 - The Simon Game/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,95 @@
+let userClickedPattern = [];
+let gamePattern = [];
+let level = 0;
+
+const buttonColors = ["red", "blue", "green", "yellow"];
+
+
+function playSound(soundfile){
+    const audio = new Audio("sounds/"+soundfile+".mp3")
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("." + currentColor).addClass("pressed");
+    setTimeout(function(){
+         $("." + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function nextSequence(){
+    const randomNumber = Math.floor(Math.random() * 4);
+
+    const pcSelectedColor = buttonColors[randomNumber];
+
+    $("." + pcSelectedColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(pcSelectedColor);
+    gamePattern.push(pcSelectedColor);
+
+    level ++;
+
+    $("#level-title").text("Level "+ level);
+
+    console.log(gamePattern);
+}
+
+function checkGame(current){
+    if(userClickedPattern[current] === gamePattern[current]){
+        if (userClickedPattern.length  === gamePattern.length){
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+            userClickedPattern = []; 
+        }
+    }
+    else{
+        $(".btn").off("click");
+        gameOver();
+    }
+}
+
+function gameOver(){
+    $("body").addClass("game-over");
+    setTimeout(function() {
+        $("body").removeClass("game-over");
+    }, 100);
+
+    $("#level-title").text("Game Over, Press 'A' to Restart");
+    playSound("wrong");
+
+    gameReset();
+    game();
+}
+
+
+function gameReset(){
+    userClickedPattern = [];
+    gamePattern = [];
+    level = 0;
+}
+function game(){
+    $(document).on("keydown", function(event){
+        if(event.key === "a"){
+            $(document).off("keydown");
+            nextSequence();
+            
+            $(".btn").on("click", function(){
+                const currentButton = this.id;
+                $("." + currentButton).addClass("pressed");
+
+                setTimeout(function() {
+                    $("." + currentButton).removeClass("pressed");
+                }, 100);
+                userClickedPattern.push(currentButton);
+
+                console.log(userClickedPattern );
+                checkGame(userClickedPattern.length - 1);       
+            });
+             
+        }
+    });
+}
+
+
+game();
