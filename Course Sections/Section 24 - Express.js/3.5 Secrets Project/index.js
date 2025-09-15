031aead259cb8f819675aad03a69993e9a578d58
Jiroh Carpio <jirohreinier.carpio@cvsu.edu.ph>
2025-09-15 21:30:35 +0800

    Completed section 3.5

--- Course Sections/Section 24 - Express.js/3.5 Secrets Project/index.js
+++ Course Sections/Section 24 - Express.js/3.5 Secrets Project/index.js
@@ -1,3 +1,42 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var returnPage = "";
+
+function checkPassword(req, res, next){
+    console.log(req.body);
+    if (req.body["password"] === "ILoveProgramming"){
+        returnPage = "secret.html"
+    }
+    else{
+        returnPage = ""
+    }
+    next();
+}
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(checkPassword);
+
+app.post("/check", (req,res) => {
+  res.sendFile(__dirname + `/public/${returnPage}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
