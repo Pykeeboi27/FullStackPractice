a0e81c6863a77a11182d270ae54b9c9cff6e968b
Jiroh Carpio <jirohreinier.carpio@cvsu.edu.ph>
2025-09-15 19:20:29 +0800

    Completed sub sections 3.1 - 3.4 (redo)

--- Course Sections/Section 24 - Express.js/3.1 Express Server/index.js
+++ Course Sections/Section 24 - Express.js/3.1 Express Server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}.`)
 });
+
--- Course Sections/Section 24 - Express.js/3.2 HTTP Requests/index.js
+++ Course Sections/Section 24 - Express.js/3.2 HTTP Requests/index.js
@@ -1,19 +1,22 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+app.get("/", (req, res)=>{
+    res.send("<h1>Hello, World!</h1>");
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+app.get("/about", (req, res)=>{
+    res.send("<h1>About Me</h1><p>Hi! My name is Jiroh.</p>");
 });
 
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+app.get("/contacts", (req, res)=>{
+    res.send("<h1>Contacts</h1><p>My Instagram: jiroh.carps</p>");
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(port, ()=>{
+    console.log(`Server is running on ${port}.`);
 });
+
+
